04389c45680ee61ee037eae319c4542d35afab37
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2024-06-26 17:06:19 -0500

    creación de la tabla , lista

--- MDBtoPG/migrator.py
+++ MDBtoPG/migrator.py
@@ -8,15 +8,31 @@ class MongoToPostgresMigrator:
         """Constructor
         """
         self.mongo_client=mongo_client
-        self.cursor=postgresql_conexion.cursor()
+        self.postgresql_conexion=postgresql_conexion
         self.mongo_db=mongo_db
         self.mongo_co=mongo_co
 
-    def migrate(self,columns_mapping=1):
+    def migrate(self,columns_mapping=1,varchar_size=50):
         if columns_mapping:
             data_mongo,columns=self._mongo_data()
         else:
             data_mongo,columns=self._mongo_data(columns_mapping)
+        data_types=self._get_dtype(data_mongo[0],varchar_size)
+        self._create_table(columns,data_types)
+        query = """
+            SELECT table_name
+            FROM information_schema.tables
+            WHERE table_schema = 'public'  -- Puedes ajustar esto según el esquema de tu base de datos
+            AND table_type = 'BASE TABLE';
+        """
+        cursor1=self.postgresql_conexion.cursor()
+        cursor1.execute(query)
+        for elem in cursor1:
+            print(elem)
+        cursor1.close()
+        cursor2=self.postgresql_conexion.cursor()
+        
+            
             
     def _get_collection(self):
         """get the collection from the mongoDB
@@ -40,17 +56,41 @@ class MongoToPostgresMigrator:
         self.mongo_client
         collection=self._get_collection()
         if columns_mapping:
-            documents=list(collection.find({}))
+            query=list(collection.find({}))
+            documents=[tuple(document.values()) for document in query]
         else:
-            documents=list(collection.find({},{column:1 for column in columns_mapping}))
+            query=list(collection.find({},{column:1 for column in columns_mapping}))
+            documents=[tuple(document.values()) for document in query]
         for elem in documents:
             for key in elem.keys():
                 if key not in columns:
                     columns.append(key)
         return documents,columns
     
-    def _get_dtype():
-        pass
-        
-    def _create_tables(self,columns):
-        pass
+    def _get_dtype(self,data:dict,varchar_size):
+        dtype=[]
+        for elem in data.values():
+            if type(elem) not in dtype:
+                if type(elem) is int:
+                    dtype.append('INTEGER')
+                elif type(elem) is float:
+                    dtype.append('FLOAT')
+                elif type(elem) is str:
+                    dtype.append('VARCHAR('+str(varchar_size)+')')
+                elif type(elem) is bool:
+                    dtype.append('BOOLEAN')
+                elif type(elem) is dict:
+                    dtype.append('json')
+                else:
+                    dtype.append('VARCHAR('+str(varchar_size)+')')
+        return dtype
+    
+    def _create_table(self,columns:list,dtypes:list):
+        cursor=self.postgresql_conexion.cursor()
+        query=f'CREATE TABLE {self.mongo_co} ('
+        for column,dtype in zip(columns,dtypes):
+            query+=column+' '+dtype+','
+        query=query[:-1]+')'
+        cursor.execute(query)
+        cursor.commit()
+        cursor.close()         
